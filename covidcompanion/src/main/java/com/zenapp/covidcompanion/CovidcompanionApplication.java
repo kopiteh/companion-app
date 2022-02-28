db8adbd4c116a632ee07374220e9e65b84308b8b
kopiteh <35788631+kopiteh@users.noreply.github.com>
2022-02-28 20:34:45 +0800

    Initial Release

---
+++ covidcompanion/src/main/java/com/zenapp/covidcompanion/CovidcompanionApplication.java
@@ -0,0 +1,13 @@
+package com.zenapp.covidcompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidcompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidcompanionApplication.class, args);
+	}
+
+}
---
+++ covidcompanion/src/test/java/com/zenapp/covidcompanion/CovidcompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenapp.covidcompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidcompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
